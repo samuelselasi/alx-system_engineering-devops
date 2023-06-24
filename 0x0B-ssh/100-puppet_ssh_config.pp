265eb95760b8218d2aefc4d90e68e047d58e22b9
samuelselasi <samelsekasi@gmail.com>
2023-06-24 21:47:54 +0000

    Add Puppet file to create ssh configuration file to turn off password and declare identity file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# file that creates ssh configuration file with Puppet
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
