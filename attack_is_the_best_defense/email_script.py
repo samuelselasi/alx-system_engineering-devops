7b038b1e9250262b88046abe5ecf0e646ac44044
samuelselasi <samelsekasi@gmail.com>
2023-06-24 01:28:12 +0000

    Add email script to test sendgrid account

---
+++ attack_is_the_best_defense/email_script.py
@@ -0,0 +1,19 @@
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+message = Mail(
+    from_email='samelsekasi@gmail.com',
+    to_emails='julien@google.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+try:
+    sg = SendGridAPIClient(os.environ.get('API-KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
