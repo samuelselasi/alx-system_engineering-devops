e4c4bdd298f210f1a471075798a66cc4921f2ff1
samuelselasi <samelsekasi@gmail.com>
2023-07-24 10:16:16 +0000

    Add a Python script to export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress and
+export data in the CSV format.
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    eid = argv[1]
+    userUrl = "https://jsonplaceholder.typicode.com/users"
+    url = userUrl + "/" + eid
+
+    response = requests.get(url)
+    uname = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(eid), 'w') as f:
+        for task in tasks:
+            f.write('"{}","{}","{}","{}"\n'
+                    .format(eid, uname, task.get('completed'),
+                            task.get('title')))
