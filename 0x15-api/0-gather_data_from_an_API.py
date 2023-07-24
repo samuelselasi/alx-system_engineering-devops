9ac52b6447daf44d4162d9d07d79ed54bcea75ee
samuelselasi <samelsekasi@gmail.com>
2023-07-24 12:22:26 +0000

    Fix alphabetical arrangement of import libraries

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,8 +4,8 @@ A Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
-from sys import argv
 import requests
+from sys import argv
 
 
 if __name__ == '__main__':
